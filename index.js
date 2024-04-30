2aaa85974d73ed0801fc6909bf2b6259f4be620e
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2024-04-30 22:26:00 +0800

    ideation is replaced
    --Now features two photos block diagram and 3d blender. About time it was used

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+            <!-- <h2>Ideation</h2>
+                <p>
+                    The conception of the idea of V.U.T.A. was a result of one of the researchers undergoing laser eye surgery. He was inspired by a customer from the clinic prior to his operation. Due to his extroverted nature, he had a conversation with a visually impaired man who told him about the struggles of impaired vision. 
+                </p>
+                <p>
+                    As it turns out, he was finding ideas to be used for thesis and had the idea of a wearable goggles that could help the blind navigate his environment in order to avoid accidents that were avoidable with healthy vision. He also considered the reuse of materials to incorporate resourcefulness and save costs using old electronics such as an empty VR headset, electronic wires, an old Raspberry Pi Cam and a 4 pack battery slot.
+                </p> -->
+            <!-- <div class="icons">
+                <img src="./images/3d_Views/Circuit Design 2.4.1.png" alt="img1">
+                <img src="./images/3d_Views/Circuit Design 2.4.1.png" alt="img2">
+                <img src="./images/3d_Views/Circuit Design 2.4.1.png" alt="img3">
+            </div> -->
+*/
